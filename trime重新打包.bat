5ba194532162c308576abcb56272c8f5a6ea4138
leimaau <229042136@qq.com>
2020-07-26 15:28:55 +0800

    update files

--- trime重新打包.bat
+++ trime重新打包.bat
@@ -1,7 +1,7 @@
 @echo off
 @echo.trime重新打包……
 
-apktool b com.osfans.trime_20181226 -o app-release-mod.apk && jarsigner -verbose -keystore debug.keystore -storepass android -signedjar app-release-signed.apk app-release-mod.apk androiddebugkey && zipalign -f -v 4 app-release-signed.apk naamning-20200630.apk
+apktool b com.osfans.trime_20181226 -o app-release-mod.apk && jarsigner -verbose -keystore debug.keystore -storepass android -signedjar app-release-signed.apk app-release-mod.apk androiddebugkey && zipalign -f -v 4 app-release-signed.apk naamning-20200726.apk
 
 echo.
 echo 执行trime重新打包完成……
