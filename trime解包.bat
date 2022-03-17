8a65e147a843ba1545f6accdeb3b80e82612d60e
leimaau <229042136@qq.com>
2022-03-17 21:22:26 +0800

    update dict

--- trime解包.bat
+++ trime解包.bat
@@ -1,7 +1,7 @@
 @echo off
 @echo.正在反编译(解包)……
 
-apktool d -f -m -s -r com.osfans.trime_20181226.apk
+apktool d -f -m -s -r app-release-25249-o_1dm0eunhm6jq1iv3rphtqt165lq-uid-574023.apk
 
 echo.
 echo 反编译(解包)完成……
