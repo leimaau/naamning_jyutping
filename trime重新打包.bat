8370ca0eebbc7e48312dd0387612d354c5774c1d
leimaau <229042136@qq.com>
2024-08-04 16:36:43 +0800

    update version

--- trime重新打包.bat
+++ trime重新打包.bat
@@ -1,7 +1,7 @@
 @echo off
 @echo.trime重新打包……
 
-apktool b app-release-25249-o_1dm0eunhm6jq1iv3rphtqt165lq-uid-574023 -o app-release-mod.apk && jarsigner -verbose -keystore debug.keystore -storepass android -signedjar app-release-signed.apk app-release-mod.apk androiddebugkey && zipalign -f -v 4 app-release-signed.apk naamning-20240408.apk
+apktool b app-release-25249-o_1dm0eunhm6jq1iv3rphtqt165lq-uid-574023 -o app-release-mod.apk && jarsigner -verbose -keystore debug.keystore -storepass android -signedjar app-release-signed.apk app-release-mod.apk androiddebugkey && zipalign -f -v 4 app-release-signed.apk naamning-20240804.apk
 
 echo.
 echo 执行trime重新打包完成……
